d7ab975e5deaed0c517e5490c1f8eae87fb12d7a
JeanBilheux <bilheuxjm@ornl.gov>
2022-09-06 10:58:03 -0400

    refactoring. this refs #4

--- src/asui/session/session_handler.py
+++ src/asui/session/session_handler.py
@@ -1,8 +1,6 @@
 from qtpy.QtWidgets import QFileDialog, QApplication
-from qtpy.QtCore import Qt
 import json
 import logging
-import numpy as np
 
 from . import SessionKeys
 from ..utilities.status_message_config import StatusMessageStatus, show_status_message
@@ -35,109 +33,6 @@ class SessionHandler:
         session_dict[SessionKeys.number_of_obs] = number_of_obs
         session_dict[SessionKeys.run_title] = run_title
 
-
-
-        # # import input tab data
-        # list_ui = self.parent.list_ui
-        #
-        # def retrieve_infos_from_import_data(data_type=DataType.projections):
-        #     folder = str(list_ui['select lineEdit'][data_type].text())
-        #     return {'folder': folder}
-        #
-        # # projections, ob, df
-        # session_dict[DataType.projections] = retrieve_infos_from_import_data(data_type=DataType.projections)
-        # session_dict[DataType.ob] = retrieve_infos_from_import_data(data_type=DataType.ob)
-        # session_dict[DataType.df] = retrieve_infos_from_import_data(data_type=DataType.df)
-        #
-        # # list of angles
-        # if not (self.parent.input['list angles'] is None):
-        #     session_dict['list angles'] = [str(angle) for angle in self.parent.input['list angles']]
-        #
-        # # output folder
-        # session_dict[DataType.output] = {'folder': str(list_ui['select lineEdit'][DataType.output].text())}
-        #
-        # # crop tab
-        # crop_state = self.parent.ui.cropping_checkBox.isChecked()
-        # # crop_width = 2*self.parent.ui.crop_width_horizontalSlider.value()
-        # crop_width = int(str(self.parent.ui.crop_width_label.text()))
-        # try:
-        #     crop_from_slice = self.parent.ui.crop_from_slice_spinBox.value()
-        #     crop_to_slice = self.parent.ui.crop_to_slice_spinBox.value()
-        # except ValueError:
-        #     crop_from_slice = np.NaN
-        #     crop_to_slice = np.NaN
-        # file_index = self.parent.ui.crop_file_index_horizontalSlider.value()
-        # crop_dict = {'state': crop_state,
-        #              'width': crop_width,
-        #              # 'width / 2': np.int(crop_width/2),
-        #              'from slice': crop_from_slice,
-        #              'to slice': crop_to_slice,
-        #              'to slice - from slice': int(np.abs(crop_to_slice - crop_from_slice + 1)),
-        #              'file index': file_index}
-        # session_dict['crop'] = crop_dict
-        #
-        # # center of rotation
-        # o_center = CenterOfRotation(parent=self.parent)
-        # center_of_rotation_state = self.parent.ui.master_center_of_rotation_checkBox.isChecked()
-        # image_0_file_index = self.parent.ui.center_of_rotation_0_degrees_comboBox.currentIndex()
-        # image_180_file_index = self.parent.ui.center_of_rotation_180_degrees_comboBox.currentIndex()
-        # algorithm_selected = o_center.get_algorithm_selected()
-        # user_value = self.parent.ui.center_of_rotation_user_defined_doubleSpinBox.value()
-        # center_of_rotation_value = o_center.get_center_of_rotation()
-        # center_rotation_dict = {'state': center_of_rotation_state,
-        #                         'image 0 file index': image_0_file_index,
-        #                         'image 180 file index': image_180_file_index,
-        #                         'algorithm selected': algorithm_selected,
-        #                         'center of rotation value': center_of_rotation_value,
-        #                         'user value': user_value}
-        # session_dict['center rotation'] = center_rotation_dict
-        #
-        # # tilt correction
-        # o_tilt = TiltHandler(parent=self.parent)
-        # tilt_correction_state = self.parent.ui.tilt_correction_checkBox.isChecked()
-        # tilt_correction_file_index = self.parent.ui.tilt_correction_file_index_horizontalSlider.value()
-        # index_of_180_degree_image = self.parent.tilt_correction_index_dict['180_degree']
-        # index_of_0_degree_image = self.parent.tilt_correction_index_dict['0_degree']
-        # tilt_algorithm = o_tilt.get_algorithm_selected()
-        # tilt_calculation = self.parent.tilt_calculation
-        # tilt_value_to_use_in_reconstruction = o_tilt.get_tilt_value_to_use_in_reconstruction()
-        # tilt_dict = {'state': tilt_correction_state,
-        #              'file index': tilt_correction_file_index,
-        #              'algorithm selected': tilt_algorithm,
-        #              'image 0 file index': index_of_0_degree_image,
-        #              'image 180 file index': index_of_180_degree_image,
-        #              TiltAlgorithm.phase_correlation: tilt_calculation[TiltAlgorithm.phase_correlation],
-        #              TiltAlgorithm.direct_minimization: tilt_calculation[TiltAlgorithm.direct_minimization],
-        #              TiltAlgorithm.use_center: tilt_calculation[TiltAlgorithm.use_center],
-        #              TiltAlgorithm.user_defined:  self.parent.ui.tilt_user_defined_doubleSpinBox.value(),
-        #              'tilt value to use in reconstruction': tilt_value_to_use_in_reconstruction,
-        #              }
-        # session_dict['tilt'] = tilt_dict
-        #
-        # # general parameters
-        # diffuseness = self.parent.ui.diffuseness_doubleSpinBox.value()
-        # smoothness = self.parent.ui.smoothness_doubleSpinBox.value()
-        # sigma = self.parent.ui.sigma_doubleSpinBox.value()
-        # o_advanced_parameters = GeneralSettingsHandler(parent=self.parent)
-        # reconstruction_algorithm = o_advanced_parameters.get_reconstruction_algorithm_selected()
-        # image_width = self.parent.image_size['width']
-        # sub_sampling_factor = self.parent.ui.sub_sampling_spinBox.value()
-        # session_dict['general parameters'] = {'diffuseness': diffuseness,
-        #                                       'smoothness': smoothness,
-        #                                       'sigma': sigma,
-        #                                       'sigma / smoothness': sigma / smoothness,
-        #                                       'reconstruction algorithm': reconstruction_algorithm,
-        #                                       'image width': image_width,
-        #                                       'sub sampling factor': sub_sampling_factor}
-        #
-        # # advanced parameters
-        # advanced_session_dict = self.parent.session_dict.get("advanced settings", None)
-        # if advanced_session_dict is None:
-        #     o_advanced = AdvancedSettingsInitialization(parent=self.parent)
-        #     o_advanced.from_config_to_session_dict()
-        # else:
-        #     session_dict['advanced settings'] = advanced_session_dict
-
         self.parent.session_dict = session_dict
 
     def load_to_ui(self):
@@ -157,61 +52,6 @@ class SessionHandler:
         run_title = session_dict.get(SessionKeys.run_title, "")
         self.parent.ui.step1_run_title_lineEdit.setText(run_title)
         self.parent.step1_run_title_changed(run_title=run_title)
-
-
-        # list_ui = self.parent.list_ui
-        #
-        # list_load_method = {DataType.projections: self.parent.projections_text_field_returned,
-        #                     DataType.ob: self.parent.ob_text_field_returned,
-        #                     DataType.df: self.parent.df_text_field_returned,
-        #                     DataType.output: self.parent.output_folder_text_field_returned}
-        #
-        # # input tab
-        # for data_type in list_load_method.keys():
-        #     _session = session_dict[data_type]
-        #     folder = _session['folder']
-        #     list_ui['select lineEdit'][data_type].setText(folder)
-        #     list_load_method[data_type]()
-        #
-        # if not (self.parent.input['data'][DataType.projections] is None):
-        #
-        #     # crop
-        #     o_crop = CropHandler(parent=self.parent)
-        #     o_crop.initialize_crop()
-        #     self.parent.ui.crop_file_index_horizontalSlider.setValue(self.parent.session_dict['crop']['file index'])
-        #     crop_state = self.parent.session_dict['crop']['state']
-        #     crop_width = self.parent.session_dict['crop']['width']
-        #     from_slice = self.parent.session_dict['crop']['from slice']
-        #     to_slice = self.parent.session_dict['crop']['to slice']
-        #
-        #     _state = Qt.Checked if crop_state else Qt.Unchecked
-        #     self.parent.ui.cropping_checkBox.setCheckState(_state)
-        #     self.parent.ui.crop_to_slice_spinBox.blockSignals(True)
-        #     self.parent.ui.crop_from_slice_spinBox.blockSignals(True)
-        #     self.parent.ui.crop_from_slice_spinBox.setValue(from_slice)
-        #     self.parent.ui.crop_to_slice_spinBox.setValue(to_slice)
-        #     self.parent.ui.crop_to_slice_spinBox.blockSignals(False)
-        #     self.parent.ui.crop_from_slice_spinBox.blockSignals(False)
-        #
-        #     self.parent.ui.crop_width_horizontalSlider.setValue(crop_width/2)
-        #     self.parent.ui.crop_width_label.setText(str(crop_width))
-        #     self.parent.ui.crop_width_horizontalSlider.setMinimum(10)
-        #     # o_crop.width_changed()
-        #     o_crop.file_index_changed()
-        #     o_crop.crop_slice_spinBox_changed(widget='all')
-        #     self.parent.crop_checkBox_clicked()
-        #
-        #     # center of rotation
-        #     o_center = CenterOfRotation(parent=self.parent)
-        #     o_center.initialize_from_session()
-        #
-        #     # tilt
-        #     o_tilt = TiltHandler(parent=self.parent)
-        #     o_tilt.initialize_tilt_from_session()
-        #
-        #     # general parameters
-        #     o_advanced = GeneralSettingsHandler(parent=self.parent)
-        #     o_advanced.initialization_from_session()
 
         show_status_message(parent=self.parent,
                             message=f"Loaded {self.config_file_name}",
