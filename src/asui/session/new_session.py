ac017db1e60c7b917a7cb7f2efff6a3d5a56796c
JeanBilheux <bilheuxjm@ornl.gov>
2022-09-14 14:11:17 -0400

    new ui. this refs #6

--- src/asui/session/new_session.py
+++ src/asui/session/new_session.py
@@ -2,11 +2,14 @@ from qtpy.QtWidgets import QDialog
 import os
 
 from .. import load_ui
+from ..step1.get import Get
+from . import SessionKeys
 
 
 class NewSession(QDialog):
 
-	def __init__(self, parent=None, config=None):
+	def __init__(self, parent=None):
+		session_dict = parent.session_dict
 		self.parent = parent
 		QDialog.__init__(self, parent=parent)
 		ui_full_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
@@ -15,6 +18,25 @@ class NewSession(QDialog):
 		self.ui = load_ui(ui_full_path, baseinstance=self)
 		self.setWindowTitle("New session")
 
+		instrument = session_dict['instrument']
+		index_instrument = self.ui.instrument_comboBox.findText(instrument)
+		self.ui.instrument_comboBox.setCurrentIndex(index_instrument)
+		self.instrument_changed(instrument)
+
+	def instrument_changed(self, new_instrument):
+		o_get = Get(parent=self.parent)
+		list_ipts = o_get.list_of_ipts(instrument=new_instrument)
+		self.ui.ipts_comboBox.clear()
+		self.ui.ipts_comboBox.blockSignals(True)
+		self.ui.ipts_comboBox.addItems(list_ipts)
+
 	def accept(self):
-		print("accepted")
+		instrument = self.ui.instrument_comboBox.currentText()
+		ipts = self.ui.ipts_comboBox.currentText()
+		ipts_index = self.ui.ipts_comboBox.selectedIndex()
+
+		self.parent.session_dict[SessionKeys.instrument] = instrument
+		self.parent.session_dict[SessionKeys.ipts_selected] = ipts
+		self.parent.session_dict[SessionKeys.ipts_index_selected] = ipts_index
+
 		self.close()
