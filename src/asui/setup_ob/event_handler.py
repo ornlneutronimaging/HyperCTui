6f2e79f4ed477e296801748b829defd1561f43c6
JeanBilheux <bilheuxjm@ornl.gov>
2022-09-15 11:55:38 -0400

    changing Ob top folder fixed. this refs #6

--- src/asui/setup_ob/event_handler.py
+++ src/asui/setup_ob/event_handler.py
@@ -9,28 +9,29 @@ from ..parent import Parent
 from .get import Get
 from ..utilities.table import TableHandler
 from ..utilities.file_utilities import list_dirs
+from ..session import SessionKeys
 
 
 class EventHandler(Parent):
 
-    def instrument_changed(self, instrument=None):
-        if instrument is None:
-            instrument = self.parent.ui.step1_instrument_comboBox.currentText()
-
-        o_get = Get(parent=self.parent)
-        list_ipts = o_get.list_of_ipts(instrument=instrument)
-        self.parent.ui.step1_ipts_comboBox.clear()
-        self.parent.ui.step1_ipts_comboBox.blockSignals(True)
-        self.parent.ui.step1_ipts_comboBox.addItems(list_ipts)
-        self.parent.session_dict['instrument'] = instrument
-        self.reset_ob_search_path()
-        self.step1_ipts_changed(ipts=list_ipts[0])
-        self.parent.ui.step1_ipts_comboBox.blockSignals(False)
-
-    def set_new_instrument(self, instrument=None):
-        new_index = self.parent.ui.step1_instrument_comboBox.findText(instrument)
-        self.parent.ui.step1_instrument_comboBox.setCurrentIndex(new_index)
-        self.instrument_changed(instrument=instrument)
+    # def instrument_changed(self, instrument=None):
+    #     if instrument is None:
+    #         instrument = self.parent.ui.step1_instrument_comboBox.currentText()
+	#
+    #     o_get = Get(parent=self.parent)
+    #     list_ipts = o_get.list_of_ipts(instrument=instrument)
+    #     self.parent.ui.step1_ipts_comboBox.clear()
+    #     self.parent.ui.step1_ipts_comboBox.blockSignals(True)
+    #     self.parent.ui.step1_ipts_comboBox.addItems(list_ipts)
+    #     self.parent.session_dict['instrument'] = instrument
+    #     self.reset_ob_search_path()
+    #     self.step1_ipts_changed(ipts=list_ipts[0])
+    #     self.parent.ui.step1_ipts_comboBox.blockSignals(False)
+	#
+    # def set_new_instrument(self, instrument=None):
+    #     new_index = self.parent.ui.step1_instrument_comboBox.findText(instrument)
+    #     self.parent.ui.step1_instrument_comboBox.setCurrentIndex(new_index)
+    #     self.instrument_changed(instrument=instrument)
 
     def run_title_changed(self, run_title=None):
         run_title_listed = run_title.split(" ")
@@ -64,8 +65,8 @@ class EventHandler(Parent):
         logging.info(f"-> clearing the list of OBs table!")
         o_table = TableHandler(table_ui=self.parent.ui.step1_open_beam_tableWidget)
         o_table.remove_all_rows()
-        ipts = self.parent.ui.step1_ipts_comboBox.currentText()
-        instrument = self.parent.ui.step1_instrument_comboBox.currentText()
+        ipts = self.parent.session_dict[SessionKeys.ipts_selected]
+        instrument = self.parent.session_dict[SessionKeys.instrument]
         full_list_path_where_to_look_for_obs = [self.parent.homepath,
                                                 instrument,
                                                 ipts,
@@ -74,14 +75,14 @@ class EventHandler(Parent):
                                                 "mcp",
                                                 ]
         full_path_where_to_look_for_obs = os.sep.join(full_list_path_where_to_look_for_obs)
-        self.parent.ui.step1_existing_ob_top_path.setText(full_path_where_to_look_for_obs)
+        self.parent.ui.existing_ob_top_path.setText(full_path_where_to_look_for_obs)
         self.parent.ui.location_of_ob_created.setText(full_path_where_to_look_for_obs)
 
     def check_state_of_ob_measured(self):
         logging.info(f"Checking the state of the OBs measured.")
 
     def browse_obs(self):
-        full_path_where_to_look_for_obs = str(self.parent.ui.step1_existing_ob_top_path.text())
+        full_path_where_to_look_for_obs = str(self.parent.ui.existing_ob_top_path.text())
         logging.info(f"Looking for OBs folders/files in {full_path_where_to_look_for_obs}")
 
         top_folder = str(QFileDialog.getExistingDirectory(self.parent,
@@ -94,19 +95,19 @@ class EventHandler(Parent):
         if not os.path.exists(top_folder):
             logging.info(f"-> folder does not exists!")
             top_folder = os.sep.join([self.parent.homepath,
-                                      self.parent.ui.step1_instrument_comboBox.currentText()])
+                                      self.parent.session_dict[SessionKeys.instrument]])
             logging.info(f"-> using {top_folder} instead!")
 
         if top_folder:
             logging.info(f"User changed top OB folder in step 1: {top_folder}")
-            self.parent.ui.step1_existing_ob_top_path.setText(top_folder)
+            self.parent.ui.existing_ob_top_path.setText(top_folder)
             self.update_list_of_obs()
 
     def update_list_of_obs(self):
         print("calling update list of obs")
         return
 
-        top_folder = self.parent.ui.step1_existing_ob_top_path.text()
+        top_folder = self.parent.ui.existing_ob_top_path.text()
         print(f"top_folder: {top_folder}")
         return
 
