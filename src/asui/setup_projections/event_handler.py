367e873707f9ad61d99f324179d62193765bf8e0
JeanBilheux <bilheuxjm@ornl.gov>
2022-10-03 10:53:08 -0400

    fixed format. this refs #28

--- src/asui/setup_projections/event_handler.py
+++ src/asui/setup_projections/event_handler.py
@@ -15,30 +15,29 @@ class EventHandler(Parent):
         run_title_listed = run_title.split(" ")
         formatted_run_title = "_".join(run_title_listed)
 
-		# show_label_ui = False
-		if checking_if_file_exists:
-			formatted_run_title, show_label_ui = self.produce_unused_formatted_run_title(formatted_run_title)
-		else:
-			show_label_ui = False
+        if checking_if_file_exists:
+            formatted_run_title, show_label_ui = self.produce_unused_formatted_run_title(formatted_run_title)
+        else:
+            show_label_ui = False
 
-		self.parent.ui.run_title_formatted_label.setText(formatted_run_title)
-		self.parent.ui.projections_title_message.setVisible(show_label_ui)
+        self.parent.ui.run_title_formatted_label.setText(formatted_run_title)
+        self.parent.ui.projections_title_message.setVisible(show_label_ui)
 
-	def produce_unused_formatted_run_title(self, run_title):
-		"""
-		this will retrieve the mcp_raw location and look if the run_title exists there.
-		if it doesn't, it will return the run_title
-		if it does, it will add an increment number
+    def produce_unused_formatted_run_title(self, run_title):
+        """
+        this will retrieve the mcp_raw location and look if the run_title exists there.
+        if it doesn't, it will return the run_title
+        if it does, it will add an increment number
 
-		return:
-			new file name, state of label telling if the file name has been changed or not
-		"""
-		o_path = self.parent.folder_path
-		mcp_raw = o_path.mcp_raw
+        return:
+            new file name, state of label telling if the file name has been changed or not
+        """
+        o_path = self.parent.folder_path
+        mcp_raw = o_path.mcp_raw
 
-		full_file_name = os.path.join(mcp_raw, run_title)
-		if not os.path.exists(full_file_name):
-			return run_title, False
+        full_file_name = os.path.join(mcp_raw, run_title)
+        if not os.path.exists(full_file_name):
+            return run_title, False
 
         mcp_raw_full_file_name = os.path.join(mcp_raw, run_title)
         autoreduce_full_file_name = os.path.join(mcp, run_title)
@@ -47,7 +46,7 @@ class EventHandler(Parent):
 
         file_increment_index = 1
         while (os.path.exists(os.path.abspath(os.path.join(mcp_raw, f"{run_title}_{file_increment_index:02d}")))) or \
-              (os.path.exists(os.path.abspath(os.path.join(mcp, f"{run_title}_{file_increment_index:02d}")))):
+                (os.path.exists(os.path.abspath(os.path.join(mcp, f"{run_title}_{file_increment_index:02d}")))):
             file_increment_index += 1
 
         return f"{run_title}_{file_increment_index:02d}", True
